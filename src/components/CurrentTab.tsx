22ece83f7c43d1cca9e615314f49f0f6b7b1ce13
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-06-17 08:17:17 +0530

    ready to deploy

--- src/components/CurrentTab.tsx
+++ src/components/CurrentTab.tsx
@@ -4,7 +4,7 @@ import { Box, Typography } from "@mui/material";
 
 type Props = { handleBack(): void; currentTab: string };
 
-const CurrentTab = ({handleBack,currentTab}: Props) => {
+const CurrentTab = ({ handleBack, currentTab }: Props) => {
   return (
     <FlexBetween gap="0.75rem">
       <Box
@@ -14,6 +14,9 @@ const CurrentTab = ({handleBack,currentTab}: Props) => {
           border: "none",
           padding: "0",
           margin: "0",
+          "&:hover": {
+            cursor: "pointer",
+          },
         }}
         display="flex"
         justifyContent="center"
@@ -22,7 +25,6 @@ const CurrentTab = ({handleBack,currentTab}: Props) => {
         <img src={backArrow} alt="back" />
       </Box>
       <Typography variant="h4" color="text.primary">
-
         {`${currentTab[0].toUpperCase()}${currentTab.slice(1)}`}
       </Typography>
     </FlexBetween>
