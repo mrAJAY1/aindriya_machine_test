a2f4c0e445e1f9edd87811ed94dbfa9026e780d5
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-06-17 07:52:23 +0530

    ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   // Defines the resolve configuration for Webpack module bundler to resolve modules.
-   resolve: {
+  // Defines the resolve configuration for Webpack module bundler to resolve modules.
+  resolve: {
     // Aliases allow to create shortcuts to import or require certain modules easily.
     alias: [
       // Defines an alias for "@" pointing to the "src" directory using path.resolve method.
@@ -14,4 +14,5 @@ export default defineConfig({
       { find: "@", replacement: path.resolve(__dirname, "src") },
     ],
   },
-})
+  base: "/aindriya_machine_test/",
+});
